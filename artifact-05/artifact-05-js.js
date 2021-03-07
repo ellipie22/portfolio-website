4ddf0ea73986c66e4e18b1f1e688d092fde24a39
ellipie22 <77076352+ellipie22@users.noreply.github.com>
2021-03-06 22:55:05 -0800

    Update artifact-05-js.js

--- artifact-05/artifact-05-js.js
+++ artifact-05/artifact-05-js.js
@@ -1,6 +1,8 @@
 var slideIndex = 1;
 showSlides(slideIndex);
-
+while(true){
+  setTimeout(plusSlides(1), 3000);
+}
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -12,8 +14,8 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {slideIndex = 1;}    
+  if (n < 1) {slideIndex = slides.length;}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
@@ -21,3 +23,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
 
 }
+
